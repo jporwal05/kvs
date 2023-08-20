cc479dd1983143c3bcfe7091e3b99e2bf0de5ef4
Jayant Porwal <jayantporwal05@gmail.com>
2023-08-20 15:39:07 +0530

    project-1

---
+++ src/bin/kvs.rs
@@ -0,0 +1,50 @@
+use std::process::exit;
+
+use clap::crate_version;
+use clap::{Arg, Command};
+
+fn main() {
+    let matches = Command::new("kvs")
+        .version(crate_version!())
+        .args([Arg::new("arg1"), Arg::new("arg2"), Arg::new("arg3")])
+        .get_matches();
+    if !matches.args_present() {
+        exit(-1)
+    }
+
+    match matches.get_one::<String>("arg1") {
+        Some(arg1) => {
+            if *arg1 == "get".to_string() {
+                match matches.get_one::<String>("arg2") {
+                    Some(_) => {
+                        eprint!("unimplemented");
+                        exit(-1);
+                    }
+                    None => panic!(),
+                }
+            } else if *arg1 == "set".to_string() {
+                match matches.get_one::<String>("arg2") {
+                    Some(_) => match matches.get_one::<String>("arg3") {
+                        Some(_) => {
+                            eprint!("unimplemented");
+                            exit(-1);
+                        }
+                        None => panic!(),
+                    },
+                    None => panic!(),
+                }
+            } else if *arg1 == "rm".to_string() {
+                match matches.get_one::<String>("arg2") {
+                    Some(_) => {
+                        eprint!("unimplemented");
+                        exit(-1);
+                    }
+                    None => panic!(),
+                }
+            } else {
+                panic!()
+            }
+        }
+        None => (),
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+/// Uses [`HashMap`] to store key-value pairs in memory.
+use std::collections::HashMap;
+
+/// A container for storing key-value pairs in memory.
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
+/// Implementation of [`KvStore`]
+impl KvStore {
+    /// Constructs a [`KvStore`]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// ```
+    pub fn new() -> KvStore {
+        KvStore {
+            store: HashMap::new(),
+        }
+    }
+
+    /// Sets a value corresponding to a key in the [`KvStore`]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set(String::from("key1"), String::from("value1"));
+    /// ```
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
+    }
+
+    /// Gets a value for a key from the [`KvStore`]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set(String::from("key1"), String::from("value1"));
+    /// store.get(String::from("key1"));
+    /// ```
+    pub fn get(&self, key: String) -> Option<String> {
+        self.store.get(&key).cloned()
+    }
+
+    /// Removes a key from the [`KvStore`]
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    ///
+    /// let mut store = KvStore::new();
+    /// store.set(String::from("key1"), String::from("value1"));
+    /// store.remove(String::from("key1"));
+    /// ```
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
+    }
+}
