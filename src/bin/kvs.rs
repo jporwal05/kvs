54b0096c062eb26f9da6e112612d27c94a759b8b
Jayant Porwal <jayantporwal05@gmail.com>
2023-08-23 18:57:11 +0530

    Implementation for cli test cases

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -2,8 +2,9 @@ use std::process::exit;
 
 use clap::crate_version;
 use clap::{Arg, Command};
+use kvs::{KvStore, Result};
 
-fn main() {
+fn main() -> Result<()> {
     let matches = Command::new("kvs")
         .version(crate_version!())
         .args([Arg::new("arg1"), Arg::new("arg2"), Arg::new("arg3")])
@@ -14,19 +15,29 @@ fn main() {
 
     if let Some(arg1) = matches.get_one::<String>("arg1") {
         if arg1 == &"get".to_string() {
+            let extra_field = matches.contains_id("arg3");
+            if extra_field {
+                panic!()
+            }
             match matches.get_one::<String>("arg2") {
-                Some(_) => {
-                    eprint!("unimplemented");
-                    exit(-1);
+                Some(arg2) => {
+                    let mut store = KvStore::open(".").unwrap();
+                    match store.get(arg2.to_string()) {
+                        Ok(value) => match value {
+                            Some(_) => (),
+                            None => println!("Key not found"),
+                        },
+                        Err(_) => (),
+                    }
                 }
                 None => panic!(),
             }
         } else if arg1 == &"set".to_string() {
             match matches.get_one::<String>("arg2") {
-                Some(_) => match matches.get_one::<String>("arg3") {
-                    Some(_) => {
-                        eprint!("unimplemented");
-                        exit(-1);
+                Some(arg2) => match matches.get_one::<String>("arg3") {
+                    Some(arg3) => {
+                        let mut store = KvStore::open(".").unwrap();
+                        store.set(arg2.to_string(), arg3.to_string()).unwrap();
                     }
                     None => panic!(),
                 },
@@ -34,9 +45,12 @@ fn main() {
             }
         } else if arg1 == &"rm".to_string() {
             match matches.get_one::<String>("arg2") {
-                Some(_) => {
-                    eprint!("unimplemented");
-                    exit(-1);
+                Some(arg2) => {
+                    let mut store = KvStore::open(".").unwrap();
+                    match store.remove(arg2.to_string()) {
+                        Ok(_) => (),
+                        Err(_) => exit(1),
+                    }
                 }
                 None => panic!(),
             }
@@ -44,4 +58,6 @@ fn main() {
             panic!()
         }
     }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,37 @@
-/// Uses [`HashMap`] to store key-value pairs in memory.
-use std::collections::HashMap;
+use std::{
+    fs::{File, OpenOptions},
+    io::{BufReader, Write},
+    path::PathBuf,
+    result,
+};
+
+use failure::Error;
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+
+/// A [`Result`] that returns type [`T`] otherwise [`Error`]
+pub type Result<T> = result::Result<T, Error>;
 
 /// A container for storing key-value pairs in memory.
 pub struct KvStore {
-    store: HashMap<String, String>,
-}
-
-impl Default for KvStore {
-    fn default() -> Self {
-        KvStore::new()
-    }
+    log: File,
 }
 
 /// Implementation of [`KvStore`]
 impl KvStore {
-    /// Constructs a [`KvStore`]
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// # use kvs::KvStore;
-    ///
-    /// let mut store = KvStore::new();
-    /// ```
-    pub fn new() -> Self {
-        KvStore {
-            store: HashMap::new(),
-        }
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
+        let mut path_buf = PathBuf::from(path.into());
+        path_buf.push("kvs.store");
+
+        let file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .read(true)
+            .open(path_buf)
+            .unwrap();
+
+        Ok(KvStore { log: file })
     }
 
     /// Sets a value corresponding to a key in the [`KvStore`]
@@ -35,12 +40,20 @@ impl KvStore {
     ///
     /// ```rust
     /// # use kvs::KvStore;
+    /// # use tempfile::TempDir;
     ///
-    /// let mut store = KvStore::new();
+    /// let mut store = KvStore::open(TempDir::new().unwrap().path()).unwrap();
     /// store.set(String::from("key1"), String::from("value1"));
     /// ```
-    pub fn set(&mut self, key: String, value: String) {
-        self.store.insert(key, value);
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let command = Command {
+            key: key.to_string(),
+            value: Some(value.to_string()),
+            command_type: CommandType::SET,
+        };
+        let command_json = serde_json::to_string(&command).unwrap();
+        self.log.write_all(command_json.as_bytes()).unwrap();
+        Ok(())
     }
 
     /// Gets a value for a key from the [`KvStore`]
@@ -49,13 +62,45 @@ impl KvStore {
     ///
     /// ```rust
     /// # use kvs::KvStore;
+    /// # use tempfile::TempDir;
     ///
-    /// let mut store = KvStore::new();
+    /// let mut store = KvStore::open(TempDir::new().unwrap().path()).unwrap();
     /// store.set(String::from("key1"), String::from("value1"));
     /// store.get(String::from("key1"));
     /// ```
-    pub fn get(&self, key: String) -> Option<String> {
-        self.store.get(&key).cloned()
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        let stream = Deserializer::from_reader(BufReader::new(&self.log)) // new line
+            .into_iter::<Command>();
+        let mut found = false;
+        let mut value: Option<String> = None;
+        for command in stream {
+            match command {
+                Ok(c) => {
+                    if c.key == key && c.command_type == CommandType::SET {
+                        found = true;
+                        value = c.value;
+                    }
+                    if c.key == key && c.command_type == CommandType::RM {
+                        found = false;
+                        value = None;
+                    }
+                }
+                Err(err) => eprintln!("{:?}", err),
+            }
+        }
+        if found {
+            let command = Command {
+                key: key.to_string(),
+                value: None,
+                command_type: CommandType::GET,
+            };
+
+            let command_json = serde_json::to_string(&command).unwrap();
+            self.log.write_all(command_json.as_bytes()).unwrap();
+            println!("{}", value.as_ref().unwrap());
+            return Ok(value);
+        }
+        Ok(None)
     }
 
     /// Removes a key from the [`KvStore`]
@@ -64,12 +109,54 @@ impl KvStore {
     ///
     /// ```rust
     /// # use kvs::KvStore;
+    /// # use tempfile::TempDir;
     ///
-    /// let mut store = KvStore::new();
+    /// let mut store = KvStore::open(TempDir::new().unwrap().path()).unwrap();
     /// store.set(String::from("key1"), String::from("value1"));
     /// store.remove(String::from("key1"));
     /// ```
-    pub fn remove(&mut self, key: String) {
-        self.store.remove(&key);
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        let stream = Deserializer::from_reader(BufReader::new(&self.log)) // new line
+            .into_iter::<Command>();
+        let mut found = false;
+        for command in stream {
+            match command {
+                Ok(c) => {
+                    if c.key == key {
+                        found = true;
+                    }
+                }
+                Err(err) => eprintln!("{:?}", err),
+            }
+        }
+
+        if found {
+            let command = Command {
+                key: key.to_string(),
+                value: None,
+                command_type: CommandType::RM,
+            };
+
+            let command_json = serde_json::to_string(&command).unwrap();
+            self.log.write_all(command_json.as_bytes()).unwrap();
+            Ok(())
+        } else {
+            println!("Key not found");
+            Err(failure::err_msg("Key not found"))
+        }
     }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Command {
+    key: String,
+    value: Option<String>,
+    command_type: CommandType,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Eq)]
+enum CommandType {
+    SET,
+    GET,
+    RM,
 }
